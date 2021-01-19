aa6fccc3af12296fa92fbdb8b00e5e6b07ce4038
Icyadam14 <48374384+techtonic14@users.noreply.github.com>
2021-01-18 23:55:37 -0500

    Initial commit

---
+++ Unit_1/test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Adam Kahl on 1/18/2021.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, !   " << std::endl;
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/CSC_175_UMFLINT.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSC_175_UMFLINT.dir/main.cpp.obj"
+  "CMakeFiles/CSC_175_UMFLINT.dir/test.cpp.obj"
+  "CSC_175_UMFLINT.exe"
+  "CSC_175_UMFLINT.exe.manifest"
+  "CSC_175_UMFLINT.pdb"
+  "libCSC_175_UMFLINT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CSC_175_UMFLINT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
