47bc31d77af608d5a20cd8918b389405e96a0728
techtonic14 <adamkahl8@gmail.com>
2021-01-23 14:42:17 -0500

    Unit 2 completed

---
+++ cmake-build-debug/CMakeFiles/2_29.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_29.exe"
+  "2_29.exe.manifest"
+  "2_29.pdb"
+  "CMakeFiles/2_29.dir/Unit_2/2_29.cpp.obj"
+  "lib2_29.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_29.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
